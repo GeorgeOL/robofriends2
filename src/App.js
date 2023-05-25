526989c8457a8c28a30f253b0c0bbe1efb506abc
GeorgeOL <george6472@gmail.com>
2023-05-26 01:24:25 +0300

    loadin and JSON

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CardList from "./CardList";
-import {robot} from "./robot";
+// import {robot} from "./robot";
 import SearchBox from './/SearchBox';
 import './App.css';
 
@@ -10,9 +10,16 @@ class App extends React.Component  {
     constructor(props) {
         super(props)
         this.state = {
-            robot: robot,
+            robot: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=>  response.json())
+            .then(users => this.setState({ robot: users}));
+
     }
 
     onSearchChange = (event) => {
@@ -26,14 +33,18 @@ class App extends React.Component  {
         const filterRobot = this.state.robot.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <dl  className={'tc'}>
-                <h1 className={'f1'}>Robofriends</h1>
-                 <h2 className={'f5'}><SearchBox searchChange={this.onSearchChange} /></h2>
-                <CardList robot={filterRobot} />
-            </dl>
+        if (this.state.robot.length === 0 ) {
+            return <h1 className={'tc f1'}>Loading</h1>
+        } else {
+            return (
+                <dl className={'tc'}>
+                    <h1 className={'f1'}>Robofriends</h1>
+                    <h2 className={'f5'}><SearchBox searchChange={this.onSearchChange}/></h2>
+                    <CardList robot={filterRobot}/>
+                </dl>
 
-        );
+            );
+        }
     }
 
 }
